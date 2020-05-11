314162f03beee112228981acfb776050f4f3904c
sindrim88 <44649144+sindrim88@users.noreply.github.com>
2020-05-11 11:30:19 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+
+
+// 2D ray casting
+
+let walls = [];
+let ray;
+let particle;
+
+function setup(){
+	createCanvas(400,400);
+	for (let i = 0; i < 5; i++){
+		let x1 = random(width);
+		let x2 = random(width);
+		let y1 = random(height);
+		let y2 = random(height);
+		walls[i] = new Boundary(x1,y1,x2,y2);
+	}
+
+	walls.push(new Boundary(0, 0, width, 0));
+	walls.push(new Boundary(width, 0, width, height));
+	walls.push(new Boundary(width, height, 0, height));
+	walls.push(new Boundary(0, height, 0, 0));
+	
+	particle = new Particle();
+}
+
+function draw(){
+	background(0);
+	for (let wall of walls){
+		wall.show();
+	}
+	
+	particle.update(mouseX, mouseY);
+	particle.show();
+	particle.look(walls);
+/*
+	//ray.show();
+	//ray.lookAt(mouseX,mouseY);
+
+
+	let pt = ray.cast(wall);
+	//console.log(pt);
+
+	if(pt){
+		fill(255);
+		//point(pt.x, pt.y);
+		ellipse(pt.x, pt.y, 8, 8);
+	}
+	*/	
+}
